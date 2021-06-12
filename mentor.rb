b57adfb23244fafedcd8284f437f98add0853e71
ryo.suzuki <ottopiza@gmail.com>
2021-06-12 10:18:04 +0000

    mentor.rb

---
+++ mentor.rb
@@ -0,0 +1,37 @@
+
+# クラス定義
+class Mentor
+
+  # インスタンスが持つ変数（値）
+  attr_accessor :name
+
+  # インスタンスを初期化するための、特別なメソッド
+  def initialize(name)
+    self.name = name
+  end
+  
+
+  # インスタンスが持つメソッド（処理）
+
+  def job
+    puts "#{self.name} です。私は現役のITプロフェッショナルです。"
+  end
+end
+class RailsMentor < Mentor
+  def initialize(name)
+    super name
+  end
+
+  def job
+    puts "#{self.name} です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end  
+    
+
+end
+# インスタンスの生成と、変数への代入
+kirameki = Mentor.new("煌木")
+
+akaide = RailsMentor.new("赤出")
+
+kirameki.job
+akaide.job
